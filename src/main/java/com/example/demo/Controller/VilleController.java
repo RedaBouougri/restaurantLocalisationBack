1d266f79a2d16b18d314d780eb872ed2a8e463a6
reda108 <redabouougri12@gmail.com>
2023-06-03 15:32:52 +0100

    j

--- src/main/java/com/example/demo/Controller/VilleController.java
+++ src/main/java/com/example/demo/Controller/VilleController.java
@@ -3,6 +3,7 @@ package com.example.demo.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import com.example.demo.service.VilleService;
 
 @RequestMapping("api/villes")
 @RestController
+@CrossOrigin("*")
 public class VilleController {
 
 	@Autowired
